2e0e97f444f8c8878e92aec1efe9adad5c8348d0
k0dm <fighttournament2@gmail.com>
2024-02-27 16:32:23 +0200

    MC-04 presentation module

--- presentation/src/main/java/com/example/presentation/core/LiveDataWrapper.kt
+++ presentation/src/main/java/com/example/presentation/core/LiveDataWrapper.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.MutableLiveData
 
 interface LiveDataWrapper<T : Any> : UpdateUi<T>, ProvideLiveData<T> {
 
-    abstract class Abstract<T : Any> : LiveDataWrapper<T> {
-
-        protected abstract val liveData: MutableLiveData<T>
+    abstract class Abstract<T : Any>(
+        protected val liveData: MutableLiveData<T>
+    ) : LiveDataWrapper<T> {
 
         override fun updateUi(value: T) {
             liveData.value = value
@@ -15,11 +15,7 @@ interface LiveDataWrapper<T : Any> : UpdateUi<T>, ProvideLiveData<T> {
         override fun liveData() = liveData
     }
 
-    abstract class Single<T : Any> : Abstract<T>() {
-        override val liveData: MutableLiveData<T> = SingleLiveEvent()
-    }
-
-    abstract class Base<T : Any> : Abstract<T>() {
-        override val liveData: MutableLiveData<T> = MutableLiveData()
-    }
+    abstract class Single<T : Any>(
+        liveData: MutableLiveData<T> = SingleLiveEvent()
+    ) : Abstract<T>(liveData)
 }
