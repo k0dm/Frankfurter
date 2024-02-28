173d939e80a37449dc50bdf19225d35a4027be54
k0dm <fighttournament2@gmail.com>
2024-02-28 20:21:37 +0200

    MC-07 Dashboard data module unit tests

--- data/src/test/java/com/example/data/dashboard/cache/CurrentDateTest.kt
+++ data/src/test/java/com/example/data/dashboard/cache/CurrentDateTest.kt
@@ -2,18 +2,43 @@ package com.example.data.dashboard.cache
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.time.LocalDateTime
 
 class CurrentDateTest {
 
     @Test
     fun test() {
-        var currentDate = CurrentDate.Base(format = "yyyy")
-        assertEquals(4, currentDate.date().length)
+        var currentDate =
+            CurrentDate.Base(date = LocalDateTime.of(2020, 11, 25, 11, 11), format = "yyyy")
+        assertEquals("2020", currentDate.date())
 
-        currentDate = CurrentDate.Base(format = "yyyy/MM")
-        assertEquals(7, currentDate.date().length)
+        currentDate =
+            CurrentDate.Base(date = LocalDateTime.of(2020, 11, 25, 11, 11), format = "yyyy/MM")
+        assertEquals("2020/11", currentDate.date())
 
-        currentDate = CurrentDate.Base(format = "yyyy/MM/dd")
-        assertEquals(10, currentDate.date().length)
+        currentDate =
+            CurrentDate.Base(date = LocalDateTime.of(2020, 11, 25, 11, 11), format = "yyyy/MM/dd")
+        assertEquals("2020/11/25", currentDate.date())
+
+        currentDate =
+            CurrentDate.Base(
+                date = LocalDateTime.of(2020, 11, 25, 11, 11),
+                format = "yyyy/MM/dd HH"
+            )
+        assertEquals("2020/11/25 11", currentDate.date())
+
+        currentDate =
+            CurrentDate.Base(
+                date = LocalDateTime.of(2020, 11, 25, 11, 11),
+                format = "yyyy/MM/dd HH:mm"
+            )
+        assertEquals("2020/11/25 11:11", currentDate.date())
+
+        currentDate =
+            CurrentDate.Base(
+                date = LocalDateTime.of(2020, 11, 25, 11, 11, 33),
+                format = "yyyy/MM/dd HH:mm:ss"
+            )
+        assertEquals("2020/11/25 11:11:33", currentDate.date())
     }
 }
