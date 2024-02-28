ca86735ed8ea5afbfacf328d4f4b0caea205fd4c
k0dm <fighttournament2@gmail.com>
2024-02-28 20:24:10 +0200

    MC-07 Dashboard data module unit tests

--- data/src/test/java/com/example/data/dashboard/cache/CurrentDateTest.kt
+++ data/src/test/java/com/example/data/dashboard/cache/CurrentDateTest.kt
@@ -9,36 +9,36 @@ class CurrentDateTest {
     @Test
     fun test() {
         var currentDate =
-            CurrentDate.Base(date = LocalDateTime.of(2020, 11, 25, 11, 11), format = "yyyy")
+            CurrentDate.Base(date = LocalDateTime.of(2020, 10, 25, 11, 13), format = "yyyy")
         assertEquals("2020", currentDate.date())
 
         currentDate =
-            CurrentDate.Base(date = LocalDateTime.of(2020, 11, 25, 11, 11), format = "yyyy/MM")
-        assertEquals("2020/11", currentDate.date())
+            CurrentDate.Base(date = LocalDateTime.of(2020, 10, 25, 11, 13), format = "yyyy/MM")
+        assertEquals("2020/10", currentDate.date())
 
         currentDate =
-            CurrentDate.Base(date = LocalDateTime.of(2020, 11, 25, 11, 11), format = "yyyy/MM/dd")
-        assertEquals("2020/11/25", currentDate.date())
+            CurrentDate.Base(date = LocalDateTime.of(2020, 10, 25, 11, 13), format = "yyyy/MM/dd")
+        assertEquals("2020/10/25", currentDate.date())
 
         currentDate =
             CurrentDate.Base(
-                date = LocalDateTime.of(2020, 11, 25, 11, 11),
+                date = LocalDateTime.of(2020, 10, 25, 11, 13),
                 format = "yyyy/MM/dd HH"
             )
-        assertEquals("2020/11/25 11", currentDate.date())
+        assertEquals("2020/10/25 11", currentDate.date())
 
         currentDate =
             CurrentDate.Base(
-                date = LocalDateTime.of(2020, 11, 25, 11, 11),
+                date = LocalDateTime.of(2020, 10, 25, 11, 13),
                 format = "yyyy/MM/dd HH:mm"
             )
-        assertEquals("2020/11/25 11:11", currentDate.date())
+        assertEquals("2020/10/25 11:13", currentDate.date())
 
         currentDate =
             CurrentDate.Base(
-                date = LocalDateTime.of(2020, 11, 25, 11, 11, 33),
+                date = LocalDateTime.of(2020, 10, 25, 11, 13, 33),
                 format = "yyyy/MM/dd HH:mm:ss"
             )
-        assertEquals("2020/11/25 11:11:33", currentDate.date())
+        assertEquals("2020/10/25 11:13:33", currentDate.date())
     }
 }
