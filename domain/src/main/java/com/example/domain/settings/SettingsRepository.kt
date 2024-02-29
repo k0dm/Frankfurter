7453e0801421576e2acf68a7c4b9e842c23121c1
Dymytryi <122440550+k0dm@users.noreply.github.com>
2024-02-29 20:37:32 +0200

    MC-09 Settings domain (#7)
    * MC-09 Settings domain
    
    * MC-09 Settings domain
    
    * MC-09 Settings domain

---
+++ domain/src/main/java/com/example/domain/settings/SettingsRepository.kt
@@ -0,0 +1,10 @@
+package com.example.domain.settings
+
+interface SettingsRepository {
+
+    suspend fun allCurrencies(): List<String>
+
+    suspend fun availableDestinations(from: String): List<String>
+
+    suspend fun save(from: String, to: String)
+}
